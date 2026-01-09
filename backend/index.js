616fe6f3fb0ecb0c122d677622d4ee7c82ff9a16
MdAklashMiah <dev.md.aklashmiah@gmail.com>
2026-01-09 16:37:47 +0600

    backend api route setup done

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
-console.log("server is running")
+const express = require("express")
+const router = require("./src/router")
+require('dotenv').config()
+const  app = express()
+
+const PORT = process.env.PORT || 5000
+
+
+app.use(router)
+
+
+app.listen(PORT, ()=>{
+
+    console.log(`server is running port is ${PORT}`)
+})
